f4861cdce750fd229184cb30c23952607a1f0087
Adriano Matos <adrianotaua@hotmail.com>
2021-09-07 22:20:04 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+function Footer() {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/adrianomatos" target="_blank" rel="noreferrer">Adriano Matos</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://www.linkedin.com/in/matttos/" target="_blank" rel="noreferrer">Me adicione no Linkedin</a></small></p>
+            </div>
+        </footer>
+
+    );
+}
+
+export default Footer;
